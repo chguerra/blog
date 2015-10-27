f6776c2ffa37d8cc8c4a47adbc8ce43fe7ce611e
load /RUBY/file.rb <chguerra@gmail.com>
2015-10-27 17:13:00 +0100

    Itineration 2
    Second itineration

--- blog.rb
+++ blog.rb
@@ -14,6 +14,9 @@ class Blog
 	def add_post(post)
 		@container << post
 	end
+	def sort_by_date
+		@container.sort!{|post1, post2| post2.date <=> post1.date}
+	end
 	def publish_front_page
 		@container.each do |post|
 			puts "#{post.title}\n************\n#{post.text}\n------------"
@@ -23,7 +26,8 @@ end
 
 
 blog = Blog.new
-blog.add_post Post.new("Post title 1","27/10/15","Post 1 text")
-blog.add_post Post.new("Post title 2","26/10/15","Post 2 text")
-blog.add_post Post.new("post title 3","23/10/15","Post 3 text")
+blog.add_post Post.new("Post title 1",'2015-10-10',"Post 1 text")
+blog.add_post Post.new("Post title 2",'2015-10-11',"Post 2 text")
+blog.add_post Post.new("post title 3",'2015-10-12',"Post 3 text")
+blog.sort_by_date
 blog.publish_front_page
